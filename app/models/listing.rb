8ef3d96b87da2a229beb431ed473b0bcef657f5e
swosty <swostypatnaik@gmail.com>
2014-09-12 11:48:03 +0530

    finish setp for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
     			
     			has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 				:storage => :dropbox,
-    			:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    			:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     			:path => ":style/:id_:filename"
     end
  end
