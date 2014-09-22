a70ece6ac01ff8d55e932b4f12b891338d7d3230
swosty <swostypatnaik@gmail.com>
2014-09-22 11:13:57 +0530

    Order pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name ,presence:true
-
-   has_many:listings, dependent: :destroy      
+   validates :name ,presence:true
+   has_many :listings, dependent: :destroy      
+   has_many :sales, class_name:"Order", foreign_key: "seller_id" 
+   has_many :purchases, class_name:"Order", foreign_key: "buyer_id" 
 end
