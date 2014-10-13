2aca4f95cc4d648bbca86ac0cdf949fa39203bfc
swosty <swostypatnaik@gmail.com>
2014-10-13 14:55:45 +0530

    transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,7 +45,11 @@ def purchases
      flash[:danger] = e.message
     end  
 
-    
+    transfer = Stripe::Transfer.create( 
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient  
+      )
 
     respond_to do |format|
       if @order.save
